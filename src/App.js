7a9743f02cf04d365be13a5b274fbaf5f1d8cc8c
itzmbm <manjunath438@gmail.com>
2021-12-01 00:11:24 +0530

    added shop page,collection-preview component and collection-item component

--- src/App.js
+++ src/App.js
@@ -3,36 +3,23 @@ import React from "react";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
-<<<<<<< HEAD
 const HatsPage = () => (
   <div>
     <h1>HATS PAGE</h1>
   </div>
 );
-||||||| 81ed469
-=======
-import HomePage from "./pages/homepage/homepage.component";
->>>>>>> ee18b8aabf55a95edc9e8c995a0925538d65dcec
+
 function App() {
-<<<<<<< HEAD
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
-||||||| 81ed469
-  return <div>Hello World!!!</div>;
-=======
-  return (
-    <div>
-      <HomePage />
-    </div>
-  );
->>>>>>> ee18b8aabf55a95edc9e8c995a0925538d65dcec
 }
 
 export default App;
